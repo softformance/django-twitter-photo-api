5be8ea63faae1dd2c8f09c1886faceb488e21b10
Dmytro Litvinov <litvinov.do.it@gmail.com>
2017-04-12 17:18:12 +0300

    Add template for correct search way

--- django_twitter_photo_api/admin.py
+++ django_twitter_photo_api/admin.py
@@ -46,7 +46,7 @@ class PostUrlForm(forms.ModelForm):
 
 class PostAdmin(admin.ModelAdmin):
 
-    change_list_template = 'admin/post_change_list.html'
+    change_list_template = 'django_twitter_photo_api/admin/post_change_list.html'
 
     list_display = ('application', 'thumb_image', 'get_username', 'caption',
                     'get_hashtags', 'created_at', 'show',)
